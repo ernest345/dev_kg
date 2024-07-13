5b5ad272af21b1d4f022908c1c61fe1e87a53672
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-07-13 19:28:02 +0600

    origin main

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Vacancy from "./components/Vacancy";
-
-
-
+import Soobshestvo from "./components/Soobshestvo";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-
         <Route path="вакансии" element={<Vacancy />} />
-
+        <Route path="сообщество" element={<Soobshestvo />} />
       </Routes>
       <Footer />
     </div>
