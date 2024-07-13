ba4c6154a72c80c5353ba63eb2510bf0d2d89fbd
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-07-13 15:24:58 +0600

    vacancy-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FaArrowRightToBracket } from "react-icons/fa6";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Modal from '../Modal';
+import Modal from "../Modal";
+import { NavLink } from "react-router-dom";
 
 const Header: React.FC = () => {
-    const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const openModal = () => setModalVisible(true);
-    const closeModal = () => setModalVisible(false);
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
 
-    return (
-        <div id="header">
-            <div className="container">
-                <div className="main-header">
-                    <img src="https://devkg.com/js/img/logo.458f2cd.svg" alt="img" />
-                    <nav>
-                        <a href="#Вакансии">Вакансии</a>
-                        <a href="#Мероприятия">Мероприятия</a>
-                        <a href="#Видео">Видео</a>
-                        <a href="#Организация">Организация</a>
-                        <a href="#Сообщество">Сообщество</a>
-                    </nav>
+  return (
+    <div id="header">
+      <div className="container">
+        <div className="main-header">
+          <img src="https://devkg.com/js/img/logo.458f2cd.svg" alt="img" />
+          <nav>
+            <NavLink to={"/вакансии"}>Вакансии</NavLink>
+            <NavLink to={"/мероприятия"}>Мероприятия</NavLink>
+            <NavLink to={"/видео"}>Видео</NavLink>
+            <NavLink to={"/организация"}>Организация</NavLink>
+            <NavLink to={"/сообщество"}>Сообщество</NavLink>
+          </nav>
 
-                    <div className="my-btn">
-                        <div className="menu" onClick={openModal}>
-                            <GiHamburgerMenu />
-                        </div>
-                        <button className='icon-btn'><FaArrowRightToBracket /></button>
-                        <button className="login-button">Войти</button>
-                    </div>
-                </div>
+          <div className="my-btn">
+            <div className="menu" onClick={openModal}>
+              <GiHamburgerMenu />
             </div>
-            <Modal show={modalVisible} handleClose={closeModal} />
+            <button className="icon-btn">
+              <FaArrowRightToBracket />
+            </button>
+            <button className="login-button">Войти</button>
+          </div>
         </div>
-    );
+      </div>
+      <Modal show={modalVisible} handleClose={closeModal} />
+    </div>
+  );
 };
 
 export default Header;
