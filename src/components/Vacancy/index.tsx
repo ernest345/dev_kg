1d0d400a8c090cf1985b083189b9e91b7a81368f
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-07-17 15:34:18 +0600

    vacancy-details

--- src/components/Vacancy/index.tsx
+++ src/components/Vacancy/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Vacancy = () => {
+  const nav = useNavigate();
   return (
     <div id="vacancy">
       <div className="container">
@@ -8,56 +10,39 @@ const Vacancy = () => {
           <div className="vacancy--btn">
             <button>Добавить вакансию</button>
           </div>
-          <div className="vacancy--job">
+          <div className="vacancy--job" onClick={() => nav("/detailVacancy")}>
             <div className="vacancy--job__title">
               <div className="vacancy--job__title--com">
                 <img
                   src="https://devkg.com/images/organizations/886e28d889f4c24ea73c66b80174110f.webp"
                   alt=""
                 />
-                <div className="">
+                <div className="" onClick={() => nav("/detailVacancy")}>
                   <h4>Компания</h4>
                   <p>Validon Agency</p>
                 </div>
               </div>
             </div>
-            <div className="vacancy--job__title">
+            <div
+              className="vacancy--job__title"
+              onClick={() => nav("/detailVacancy")}
+            >
               <h4>Должность</h4>
               <p>Team Leader</p>
             </div>
-            <div className="vacancy--job__title">
+            <div
+              className="vacancy--job__title"
+              onClick={() => nav("/detailVacancy")}
+            >
               <h4>Оклад</h4>
               <p>От 90000 RUB в месяц</p>
             </div>
-            <div className="vacancy--job__title">
+            <div
+              className="vacancy--job__title"
+              onClick={() => nav("/detailVacancy")}
+            >
               <h4>Тип</h4>
               <p>Удаленная работа</p>
-            </div>
-          </div>
-          <div className="vacancy--job">
-            <div className="vacancy--job__title">
-              <div className="vacancy--job__title--com">
-                <img
-                  src="https://devkg.com/images/organizations/5a789fb32d0be7c268826a3e2e6a9c6a.webp"
-                  alt=""
-                />
-                <div className="">
-                  <h4>Компания</h4>
-                  <p>Tomorrow-Tech</p>
-                </div>
-              </div>
-            </div>
-            <div className="vacancy--job__title">
-              <h4>Должность</h4>
-              <p>Стажер Ux Ui</p>
-            </div>
-            <div className="vacancy--job__title">
-              <h4>Оклад</h4>
-              <p>Неоплачиваемая/мес</p>
-            </div>
-            <div className="vacancy--job__title">
-              <h4>Тип</h4>
-              <p>Стажировка/офис</p>
             </div>
           </div>
           <div className="vacancy--btn2">
