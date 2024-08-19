bcb1fd7786376c3705be03ba73d8f13df04b5167
ernest345 <0704439069bh@gmail.com>
2024-08-19 08:13:15 +0600

    integrate-vacancy

--- src/components/Videoo/index.tsx
+++ src/components/Videoo/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Videoo() {
   return (
-    <div>
+    <div className="video">
+      <div className="container">
+        <div className="video--header">
+          <button>Добавить видео</button>
+          
+        </div>
+      </div>
     </div>
   );
 }
